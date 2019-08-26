a9383054fda2de9c2f757f775a5e9ff8b034ee71
qinshuangshuang <qinshuangshuang@shouqiev.com>
2019-08-26 18:42:01 +0800

    test

---
+++ xiaoran-infrastructure/src/main/java/com/xiaoran/infrastructure/XiaoranInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoran.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoranInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoranInfrastructureApplication.class, args);
+    }
+
+}
---
+++ xiaoran-infrastructure/src/test/java/com/xiaoran/infrastructure/XiaoranInfrastructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoran.infrastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiaoranInfrastructureApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
