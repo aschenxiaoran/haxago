627a00fbe57354593c6eaf212426dd7a233a867f
qinshuangshuang <qinshuangshuang@shouqiev.com>
2019-08-26 18:52:15 +0800

    add user controller

---
+++ xiaoran-infrastructure/src/main/java/com/xiaoran/infrastructure/AppEvent.java
@@ -0,0 +1,18 @@
+package com.xiaoran.infrastructure;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * @ClassName AppEvent
+ * Description TODO
+ * Author xiaoran
+ * Date 2019/8/26 18:48
+ * Version 1.0
+ **/
+public class AppEvent implements ApplicationListener<ApplicationEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+
+    }
+}
---
+++ xiaoran-infrastructure/src/main/java/com/xiaoran/infrastructure/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.xiaoran.infrastructure.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName UserController
+ * Description TODO
+ * Author xiaoran
+ * Date 2019/8/26 18:49
+ * Version 1.0
+ **/
+@Controller
+public class UserController {
+}
