1ebc53fe5a857f781142822a47bacfe0e1493635
qinshuangshuang <qinshuangshuang@shouqiev.com>
2019-08-27 17:45:09 +0800

    add user controller

---
+++ xiaoran-infrastructure/src/main/java/com/xiaoran/infrastructure/config/annotation/AggragateRoot.java
@@ -0,0 +1,9 @@
+package com.xiaoran.infrastructure.config.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AggragateRoot {
+}
